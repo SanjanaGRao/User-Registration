03bb1c88799667be0dbda60e96f5f443551bebde
Sanjana Rao <sanjanagrao99@gmail.com>
2021-09-06 19:45:50 +0530

    Added: Ability to validate the mobile number entered by the user

--- UserRegistration.java
+++ UserRegistration.java
@@ -13,8 +13,8 @@ public class UserRegistration
 {
 	public static Scanner sc = new Scanner(System.in);
 	public final String firstNamePattern = "^[A-Z]{1}[a-z]{2,}$";
-	public final String lastNamePattern = "^[A-Z]{1}[a-z]{2,}$";
-	public final String emailPattern = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.(co|com)(.[a-z])?$";
+	public final String emailPattern = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.(co|com|edu)(.[a-z])?$";
+	public final String mobileNoPattern = "^[0-9]{2}[0-9]{3,12}$";
 	
 	//Constructor to display welcome message 
 	public UserRegistration()
@@ -26,6 +26,8 @@ public class UserRegistration
 	 * Method firstNameCheck takes parameter firstName from the user
 	 * UC 1 - First name starts with Cap and has minimum 3 characters - check 
 	 * First name starts with Cap and have minimum 3 characters.
+	 * UC 2 - Last name starts with Cap and has minimum 3 characters - check 
+	 * Last name starts with Cap and have minimum 3 characters.
 	 * @returns true or false
 	 */
 	public boolean firstNameCheck(String firstName)
@@ -35,19 +37,8 @@ public class UserRegistration
 		return s;
 	}
 	/*
-	 * Method lastNameCheck takes parameter lastName from the user
-	 * UC 2 - Last name starts with Cap and has minimum 3 characters - check 
-	 * Last name starts with Cap and have minimum 3 characters.
-	 * @returns true or false
-	 */
-	public boolean lastNameCheck(String lastName)
-	{
-		Pattern newPattern = Pattern.compile(lastNamePattern);
-		return newPattern.matcher(lastName).matches();
-	}
-	/*
 	 * Method emailCheck takes parameter email from the user
-	 * UC 2 - enter a valid email - E.g. abc.xyz@bl.co.in 
+	 * UC 3 - enter a valid email - E.g. abc.xyz@bl.co.in 
 	 * Email has 3 mandatory parts (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
 	 * @returns true or false
 	 */
@@ -56,7 +47,18 @@ public class UserRegistration
 		Pattern newPattern = Pattern.compile(emailPattern);
 		return newPattern.matcher(email).matches();
 	}
-
+	/*
+	 * Method MobileNoCheck takes parameter mobileNo from the user
+	 * UC 4 - Mobile Format - E.g. 91 9919819801 
+	 * Country code follow by space and 10 digit number 
+	 * @returns true or false
+	 */
+	public boolean mobileNoCheck(String mobileNo) 
+	{
+		Pattern newPattern = Pattern.compile(mobileNoPattern);
+		return newPattern.matcher(mobileNo).matches();
+	}	
+	
 	// This is the main function
 	public static void main(String[] args)
 	{
@@ -73,7 +75,7 @@ public class UserRegistration
 		{
 			System.out.println("The First Name entered is Invalid.");
 		}
-		if(user.lastNameCheck(lastName)) 
+		if(user.firstNameCheck(lastName)) 
 		{
 			System.out.println("The Last Name entered is Valid.");
 		} 
@@ -81,6 +83,7 @@ public class UserRegistration
 		{
 			System.out.println("The Last Name entered is Invalid.");
 		}
+		
 		System.out.println("Enter your Email ID: ");
 		String email = sc.next();
 		if(user.emailCheck(email)) 
@@ -91,5 +94,17 @@ public class UserRegistration
 		{
 			System.out.println("The EMail ID entered is Invalid.");
 		}
-	}	
+		
+		System.out.println("Enter your Mobile Number with the Country code: ");
+		String mobileNo = sc.next();
+		if(user.mobileNoCheck(mobileNo)) 
+		{
+			System.out.println("The Mobile number entered is Valid.");
+		} 
+		else 
+		{
+			System.out.println("The Mobile number entered is Invalid.");
+		}
+	}
+
 }
